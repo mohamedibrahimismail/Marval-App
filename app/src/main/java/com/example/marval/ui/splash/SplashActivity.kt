74e0bf292da78884a92f6ee34978a39df6a8a989
mohamedibrahimismail <mieibrahim15@gmail.com>
2021-04-12 17:50:43 +0200

    adding splash

--- app/src/main/java/com/example/marval/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/marval/ui/splash/SplashActivity.kt
@@ -3,18 +3,26 @@ package com.example.marval.ui.splash
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.marval.ui.base.BaseActivity
 import com.example.marval.R
 import com.example.marval.ui.main.MainActivity
+import jp.wasabeef.glide.transformations.BlurTransformation
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : BaseActivity() {
     override fun getActivityView(): Int = R.layout.activity_splash
 
     override fun afterInflation(savedInstance: Bundle?) {
+        Glide.with(this).load(R.drawable.mcu_background)
+            .apply(RequestOptions.bitmapTransform(BlurTransformation(25, 3)))
+            .into(backgroundView)
+
         Handler().postDelayed({
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
             this@SplashActivity.finish()
-        }, 2000)
+        }, 3000)
     }
 
 }
