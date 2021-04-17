b2db1057d7c7c4f649484e55b7ea042245d33bef
mohamedibrahimismail <mieibrahim15@gmail.com>
2021-04-18 00:29:30 +0200

    adding some features

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ in this project i used several technologies to achieve best performance , i used
 - Koin for dependency injection
 - Retrofite for calling apis
 - Using RXjava tools (PublishRelay with debounce)to manage duplicate or unneeded requests in autocomplete search
+- Using ParallaxRecyclerView third library Api to Implement a parallax effect to the characters’ cells when scrolling
  
 
 and considered clean code , easy-to-read code , user-friendly UI and bug-free code.
--- app/src/main/java/com/example/marval/ui/main/MainActivityAdapter.kt
+++ app/src/main/java/com/example/marval/ui/main/MainActivityAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.marval.ui.main
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.marval.R
@@ -8,26 +9,30 @@ import com.example.marval.model.main.Result
 import com.example.marval.ui.base.CommonVH
 import com.example.marval.ui.details.DetailsActivity
 import com.example.marval.utils.bindImage
+import com.yayandroid.parallaxrecyclerview.ParallaxImageView
+import com.yayandroid.parallaxrecyclerview.ParallaxViewHolder
 import kotlinx.android.synthetic.main.main_recycler_item.view.*
 import org.jetbrains.anko.sdk27.coroutines.onClick
 
+
 private const val TAG = "MainActivityAdapter"
 
-class MainActivityAdapter() : RecyclerView.Adapter<CommonVH>() {
+class MainActivityAdapter() : RecyclerView.Adapter<MainActivityAdapter.BasicViewHolder>() {
 
     var list: MutableList<Result> = arrayListOf()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommonVH {
-        return CommonVH(
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) :BasicViewHolder {
+        return BasicViewHolder(
             LayoutInflater.from(parent.context)
                 .inflate(R.layout.main_recycler_item,  parent, false)
         )
     }
 
-    override fun onBindViewHolder(holder: CommonVH, position: Int) {
+    override fun onBindViewHolder(holder: BasicViewHolder, position: Int) {
         with(holder.itemView) {
             character_name_txt.text = list.get(position).name
             image.bindImage(list.get(position).thumbnail.path + "." + list.get(position).thumbnail.extension)
+            image.reuse()
             this.onClick {
                 context.startActivity(DetailsActivity.getIntent(context, list.get(position)))
             }
@@ -46,5 +51,13 @@ class MainActivityAdapter() : RecyclerView.Adapter<CommonVH>() {
     override fun getItemCount(): Int {
         //return languages.size
         return list.size
+    }
+
+    class BasicViewHolder(itemView: View?) : ParallaxViewHolder(itemView) {
+
+        override fun getParallaxImageId(): Int {
+            return R.id.image
+        }
+
     }
 }
